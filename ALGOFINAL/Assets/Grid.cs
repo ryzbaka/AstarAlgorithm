fb289e62bbe0ec1b1077bde8c16972aae8eb8d0c
Hamza Ali <hamzaali151214@hotmail.com>
2018-12-06 20:49:55 +0530

    Update Grid.cs

--- ALGOFINAL/Assets/Grid.cs
+++ ALGOFINAL/Assets/Grid.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,9 +19,9 @@ public class Grid : MonoBehaviour {
 		gridSizeY =Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 		CreateGrid();
 	}
-	void CreateGrid()
+	void CreateGrid()//fills nodes in the grid
 	{
-		grid = new Node[gridSizeX, gridSizeY];
+		grid = new Node[gridSizeX, gridSizeY];//values taken from Inspector
 		Vector3 worldBottomLeft=transform.position-Vector3.right*gridWorldSize.x/2-Vector3.forward *gridWorldSize.y/2;
 
 		for (int x=0;x<gridSizeX;x++)
@@ -29,8 +29,10 @@ public class Grid : MonoBehaviour {
 			for(int y=0;y<gridSizeY;y++)
 			{
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				//Vector3.right is a way of writing (1,0,0) and Vector3.forward is (0,0,1) since we're moving in the x-z plane
 				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius,unwalkableMask));
-				grid[x, y] = new Node(walkable, worldPoint,x,y);
+				//Physics.CheckSphere(centre,search_radius,layer) returns 1 if layer found within radius
+				grid[x, y] = new Node(walkable, worldPoint,x,y);//assign a node to the position
 			}
 		}
 	}
@@ -45,10 +47,11 @@ public class Grid : MonoBehaviour {
 				{
 					continue;
 				}
+				//adding coordinate to reference node for searching
 				int checkX = node.gridX + x;
 				int checkY = node.gridY + y;
 
-				if(checkX>=0 && checkX<gridSizeX && checkY>=0 && checkY<gridSizeY)
+				if(checkX>=0 && checkX<gridSizeX && checkY>=0 && checkY<gridSizeY)//checks if the node being searched exists in the grid or not
 				{
 					neighbours.Add(grid[checkX, checkY]);
 				}
@@ -56,7 +59,7 @@ public class Grid : MonoBehaviour {
 		}
 		return neighbours;
 	}
-	public Node NodeFromWorldPoint(Vector3 worldPosition)//used to return a 
+	public Node NodeFromWorldPoint(Vector3 worldPosition)//used to convert an xyz coordinate to a node coordinate 
 	{
 		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
 		float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;//check y and z values later
@@ -68,28 +71,29 @@ public class Grid : MonoBehaviour {
 		//this'll become more clear once you try out the maths used above on paper
 		return grid[x,y];
 	}
-	public List<Node> path;
-	void OnDrawGizmos()
+	public List<Node> path;//values initialised in Pathfinding.cs script
+	void OnDrawGizmos()//used to visualise the algorithm's working in 3d space
 	{
-		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
-		if(grid!=null)
+		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));//draws the boundary of the grid
+		if(grid!=null)// Checks if nodes are present in grid or not
 		{
-			Node playerNode = NodeFromWorldPoint(player.position);
-			foreach(Node n in grid)
+			Node playerNode = NodeFromWorldPoint(player.position);//takes player's xyz coordinate converts to node coordinate and assigns to a variable
+			foreach(Node n in grid) //a variation of for loop where you can simply traverse through game objects
 			{
-				if(n.walkable)
+				//setting color of nodes
+				if(n.walkable)//walkable node= white
 				{
 					Gizmos.color = Color.white;
 				}
-				else
+				else// non walkable node =red
 				{
 					Gizmos.color = Color.red;
 				}
-				if(playerNode==n)
+				if(playerNode==n)//player position node = cyan
 				{
 					Gizmos.color = Color.cyan;
 				}
-				if(path!=null)
+				if(path!=null)//path colour = black
 				{
 					if(path.Contains(n))
 					{
@@ -97,8 +101,8 @@ public class Grid : MonoBehaviour {
 					}
 
 				}
-				Gizmos.DrawCube(n.worldposition, Vector3.one * (nodeDiameter - 0.1f));
+				Gizmos.DrawCube(n.worldposition, Vector3.one * (nodeDiameter - 0.1f));//draw a node cube of corresponding color
 			}
 		}
 	}
-}
+}//end of grid class
