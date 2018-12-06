ab37bd49af1cee3367fb70e5544e13832b5843fa
Hamza Ali <hamzaali151214@hotmail.com>
2018-12-06 20:52:09 +0530

    Update movementscript.cs

--- ALGOFINAL/Assets/movementscript.cs
+++ ALGOFINAL/Assets/movementscript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +6,7 @@ public class movementscript : MonoBehaviour {
 	public float myspeed;
 	// Use this for initialization
 	void Start () {
-		myspeed = 50f;
+		myspeed = 10f;
 	}
 	
 	// Update is called once per frame
